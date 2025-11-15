3d955be3abf9ae7abc390c9e0a38f1ff798fdfa2
DiegoCico <cicotosted@gmail.com>
2025-11-14 19:01:02 -0500

    eslist working

--- A5/kanbas-next-app/app/Kambaz/Courses/[cid]/Modules/page.tsx
+++ A5/kanbas-next-app/app/Kambaz/Courses/[cid]/Modules/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 "use client";
 
 import { FormControl, ListGroup, ListGroupItem } from "react-bootstrap";
--- A5/kanbas-next-app/app/Labs/Lab5/client.ts
+++ A5/kanbas-next-app/app/Labs/Lab5/client.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import axios from "axios";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export const fetchWelcomeMessage = async () => {
